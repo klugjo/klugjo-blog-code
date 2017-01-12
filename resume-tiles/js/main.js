5246cba1ef0baf9aff9445bba61a42550e7dc98d
Klughertz Jonathan <jonathan.klughertz@ip-10-2-1-253.ap-southeast-1.compute.internal>
2017-01-12 15:55:40 +0800

    Popup on click working

--- resume-tiles/js/main.js
+++ resume-tiles/js/main.js
@@ -1,12 +1,28 @@
+var appData = {
+    cards: [
+        { title: 'Docker', iconCssClass: 'devicon-docker-plain' },
+        { title: 'React', iconCssClass: 'devicon-react-original' },
+        { title: 'JavaScript', iconCssClass: 'devicon-javascript-plain' },
+        { title: 'OSX', iconCssClass: 'ion-social-apple' },
+        { title: 'Angular', iconCssClass: 'devicon-angularjs-plain' },
+    ]
+};
+
+
 new Vue({
     el: '#app',
     data: {
-        cards: [
-            { title: 'Docker', iconCssClass: 'devicon-docker-plain' },
-            { title: 'React', iconCssClass: 'devicon-react-original' },
-            { title: 'JavaScript', iconCssClass: 'devicon-javascript-plain' },
-            { title: 'OSX', iconCssClass: 'ion-social-apple' },
-            { title: 'Angular', iconCssClass: 'devicon-angularjs-plain' },
-        ]
+        cards: appData.cards,
+        cardEnlargedIndex: null
+    },
+    methods: {
+        onCardClick: function (cardIndex) {
+            if (this.cardEnlargedIndex === null) {
+                this.cardEnlargedIndex = cardIndex;
+            } else { 
+                this.cardEnlargedIndex = null;
+            }
+            
+        }
     }
 });
