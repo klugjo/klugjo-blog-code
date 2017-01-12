c4821e7cfc8e4c182aaddc79a56729b317f2ef59
Klughertz Jonathan <jonathan.klughertz@ip-10-2-1-253.ap-southeast-1.compute.internal>
2017-01-12 12:17:44 +0800

    Replace jquery with Vue.JS

--- resume-tiles/js/main.js
+++ resume-tiles/js/main.js
@@ -1,5 +1,12 @@
-$(document).ready(function () {
-    $('.card').click(function () {
-        $(this).toggleClass('flipped');
-     });
+new Vue({
+    el: '#app',
+    data: {
+        cards: [
+            { title: 'Docker', iconCssClass: 'devicon-docker-plain' },
+            { title: 'React', iconCssClass: 'devicon-react-original' },
+            { title: 'JavaScript', iconCssClass: 'devicon-javascript-plain' },
+            { title: 'OSX', iconCssClass: 'ion-social-apple' },
+            { title: 'Angular', iconCssClass: 'devicon-angularjs-plain' },
+        ]
+    }
 });
