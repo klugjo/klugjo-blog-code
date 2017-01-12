a232208113b5ae041d0da7cd2e33afe96341db32
Klughertz Jonathan <jonathan.klughertz@ip-10-2-1-253.ap-southeast-1.compute.internal>
2017-01-12 17:43:48 +0800

    use GSAP for animation

--- resume-tiles/js/main.js
+++ resume-tiles/js/main.js
@@ -5,24 +5,29 @@ var appData = {
         { title: 'JavaScript', iconCssClass: 'devicon-javascript-plain' },
         { title: 'OSX', iconCssClass: 'ion-social-apple' },
         { title: 'Angular', iconCssClass: 'devicon-angularjs-plain' },
-    ]
+    ],
+    popupCss: {
+        width: '50vw',
+        height: '50vh',
+        top: '50%',
+        left: '50%',
+        'margin-top': '-25vh',
+        'margin-left': '-25vw'
+    }
 };
-
 
 new Vue({
     el: '#app',
     data: {
         cards: appData.cards,
-        cardEnlargedIndex: null
+        cardEnlargedIndex: null,
+        cardsDOM: document.getElementsByClassName('card')
     },
     methods: {
         onCardClick: function (cardIndex) {
-            if (this.cardEnlargedIndex === null) {
-                this.cardEnlargedIndex = cardIndex;
-            } else { 
-                this.cardEnlargedIndex = null;
-            }
-            
+            var cardDOM = this.cardsDOM[cardIndex];
+            cardDOM.style['z-index'] = 10;
+            TweenLite.to(cardDOM, 0.3, { css: appData.popupCss });
         }
     }
 });
