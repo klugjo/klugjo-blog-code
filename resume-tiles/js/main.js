b0fbb2d9b051fba7650e8e6c017677ac70cf3df8
Klughertz Jonathan <jonathan.klughertz@ip-10-2-1-253.ap-southeast-1.compute.internal>
2017-01-13 12:49:56 +0800

    Open animation smooth

--- resume-tiles/js/main.js
+++ resume-tiles/js/main.js
@@ -1,33 +1,83 @@
-var appData = {
-    cards: [
+var resumeApp = function () {
+    var cards = [
         { title: 'Docker', iconCssClass: 'devicon-docker-plain' },
         { title: 'React', iconCssClass: 'devicon-react-original' },
         { title: 'JavaScript', iconCssClass: 'devicon-javascript-plain' },
         { title: 'OSX', iconCssClass: 'ion-social-apple' },
         { title: 'Angular', iconCssClass: 'devicon-angularjs-plain' },
-    ],
-    popupCss: {
-        width: '50vw',
-        height: '50vh',
-        top: '50%',
-        left: '50%',
-        'margin-top': '-25vh',
-        'margin-left': '-25vw'
+    ];
+
+    var getViewportSize = function () {
+        var e = window;
+        var a = 'inner';
+        if (!('innerWidth' in window)) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        return { width: e[a + 'Width'], height: e[a + 'Height'] };
+    };
+
+    var getPopupLayoutCss = function (cardDOM) { 
+        var viewport = getViewportSize();
+        var cardCoordinates = cardDOM.getBoundingClientRect();
+        var cardMarginTop = Math.round((viewport.height / 4) - cardCoordinates.top);
+        var cardMarginLeft = Math.round((viewport.width / 4) - cardCoordinates.left);
+
+        return {
+            height: (viewport.height / 2) + 'px',
+            width: (viewport.width / 2) + 'px',
+            'margin-top': cardMarginTop + 'px',
+            'margin-left': cardMarginLeft + 'px'
+        };
     }
-};
+
+    var enlargeCard = function (opts) { 
+        var cardIndex = opts.cardIndex;
+        var cardsDOM = document.getElementsByClassName('card');
+        var flipCard = opts.flipCard;
+        var cardDOM = cardsDOM[cardIndex];
+
+        var onReverseComplete = (function () {
+            flipCard(null);
+            cardDOM.style['z-index'] = 1;
+        }).bind(this);
+
+        flipCard(cardIndex);
+        cardDOM.style['z-index'] = 10;
+
+        return TweenMax.to(cardDOM, 0.3, {
+            css: getPopupLayoutCss(cardDOM),
+            onReverseComplete: onReverseComplete,
+            ease: Back.easeOut.config(2)
+        });
+    };
+
+    return {
+        cards: cards,
+        enlargeCard: enlargeCard
+    }
+}();
 
 new Vue({
     el: '#app',
     data: {
-        cards: appData.cards,
-        cardEnlargedIndex: null,
-        cardsDOM: document.getElementsByClassName('card')
+        cards: resumeApp.cards,
+        openCardIndex: null,
+        tweenOpenCard: null
     },
     methods: {
+        flipCard: function (cardIndex) { 
+            this.openCardIndex = cardIndex;
+        },
         onCardClick: function (cardIndex) {
-            var cardDOM = this.cardsDOM[cardIndex];
-            cardDOM.style['z-index'] = 10;
-            TweenLite.to(cardDOM, 0.3, { css: appData.popupCss });
+            if (this.openCardIndex === null) {
+                this.tweenOpenCard = resumeApp.enlargeCard({
+                    cardIndex: cardIndex,
+                    flipCard: this.flipCard
+                });
+            } else { 
+                this.tweenOpenCard.reverse();
+            }
         }
     }
 });
